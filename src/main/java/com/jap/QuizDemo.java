8c5af13453bebc192f9f6d74cb126b1ca8f6d57e
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-25 12:29:04 +0530

    Create method convertAllNames to capital and implemented

--- src/main/java/com/jap/QuizDemo.java
+++ src/main/java/com/jap/QuizDemo.java
@@ -5,15 +5,41 @@ public class QuizDemo {
     //return the school name which has got the highest score in quiz
     //Handle the NumberFormatException
     public String highestScore(String []nameOfSchool , String[] scores ){
+        String highsScore=null;
+        try {
+
+            for (int i = 0; i < scores.length; i++) {
+                if (Integer.parseInt(scores[i])<Integer.parseInt(scores[i+1])){
+                    i=i+1;
+                    for (int j = 0; j < nameOfSchool.length; j++) {
+                        highsScore=nameOfSchool[i];
+
+                    }
+                }
+
+            }
+        }
+        catch (NumberFormatException e){
+           return e.toString();
+        }
 
 
-        return "";
+        return highsScore;
 
     }
     //convert all the names in uppercase
     //Handle the NullPointerException
     public String[] convertAllNamesToCapital(String name[]){
-        String upperCase[] = new String[0];
+        String upperCase[] = new String[name.length];
+        try {
+            for (int i = 0; i < name.length; i++) {
+                upperCase[i]=name[i].toUpperCase();
+
+            }
+        }
+        catch (NullPointerException e){
+            e.toString();
+        }
 
         return upperCase;
     }
